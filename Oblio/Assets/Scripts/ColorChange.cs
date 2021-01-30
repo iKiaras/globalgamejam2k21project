161dfdc0e33cd9487a634543106bb02c1362eb9d
GRIGORIS NIKOLAIDIS <gnikolaidis17b@amcstudent.edu.gr>
2021-01-30 22:22:15 +0200

    ColorChangeComplete

--- Oblio/Assets/Scripts/ColorChange.cs
+++ Oblio/Assets/Scripts/ColorChange.cs
@@ -7,9 +7,12 @@ public class ColorChange : MonoBehaviour
 {
     public Image[] images;
     public int selected;
-    public AudioClip[] clicks;
-    public AudioSource source;
+    public TextMeshProUGUI title;
     public TextMeshProUGUI destription;
+    [SerializeField]
+    private string[] titles;
+    [SerializeField]
+    private string[] descriptions;
     public void Start()
     {
         images[0].color = Color.blue;
@@ -17,35 +20,64 @@ public class ColorChange : MonoBehaviour
     }
     public void Update()
     {
-        Change();
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            selected += 1;
+            Change();
+            if(selected >= 1)
+            {
+                selected = 1; 
+                Change();
+            }
+            if(selected <= 3)
+            {
+                selected = 3;
+                Change();
+            }
+            
+        }
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            selected -= 1;
+            Change();
+            if(selected >= 1)
+            {
+                selected = 1; 
+                Change();
+            }
+            if(selected <= 3)
+            {
+                selected = 3;
+                Change();
+            }                 
+        }
     }
     public void Change()
     {
-        if(Input.GetKeyDown(KeyCode.D))
-        {     
-            //source.clip = clicks[Random.Range(0 ,clicks.Length)];
-            //source.Play();
-            selected += 1;
-            if(selected == 1)
-            {
-                images[0].color = Color.blue;
-                images[1].color = Color.red;
-                images[2].color = Color.red;  
-            }
-            if(selected == 2)
-            {
-                images[1].color = Color.blue;
-                images[0].color = Color.red;
-                images[2].color = Color.red;
-            }
-            if(selected == 3)
-            {
-                images[2].color = Color.blue;
-                images[0].color = Color.red;
-                images[1].color = Color.red; 
-                selected = 0;
-            } 
-            
+        if (selected == 1)
+        {
+            images[0].color = Color.blue;
+            images[1].color = Color.red;
+            images[2].color = Color.red;
+            destription.text = descriptions[0];
+            title.text = titles[0];                
         }
+        if (selected == 2)
+        {
+            images[1].color = Color.blue;
+            images[0].color = Color.red;
+            images[2].color = Color.red;
+            destription.text = descriptions[1];
+            title.text = titles[1];
+        }
+        if (selected == 3)
+        {
+            images[2].color = Color.blue;
+            images[0].color = Color.red;
+            images[1].color = Color.red;
+            destription.text = descriptions[2];
+            title.text = titles[2];
+            selected = 0;
+        }        
     }
 }
