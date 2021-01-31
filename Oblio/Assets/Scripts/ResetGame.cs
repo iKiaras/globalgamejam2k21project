149275615263637a0ddc212ebaca61003f8fb625
Ioannis kiaras <ioannis.kiaras@commxp.io>
2021-01-31 19:02:20 +0200

    Final

---
+++ Oblio/Assets/Scripts/ResetGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("firstStageClear", 0);
+        PlayerPrefs.SetInt("secondStageClear", 0);
+        PlayerPrefs.SetInt("thirdStageClear", 0);
+        PlayerPrefs.Save();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
