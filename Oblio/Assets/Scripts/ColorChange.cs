cc51e49a44a17c35a2034d6ec1998ce508232086
Ioannis kiaras <ioannis.kiaras@commxp.io>
2021-01-31 17:04:36 +0200

    Done

--- Oblio/Assets/Scripts/ColorChange.cs
+++ Oblio/Assets/Scripts/ColorChange.cs
@@ -9,37 +9,17 @@ public class ColorChange : MonoBehaviour
 {
     public Image[] images;
     public int selected;
+    public TextMeshProUGUI title;
     public TextMeshProUGUI destription;
     [SerializeField]
     private string[] descriptions;
-    private bool firstStageClear = false;
-    private bool secondStageClear= false;
-    private bool thirdStageClear = false;
-    private bool sceneTransitioning = false;
+
+    public bool sceneTransitioning = false;
     public void Start()
     {
         images[0].color = Color.blue;
         selected = 1;
         destription.text = descriptions[0];
-        if (PlayerPrefs.GetInt("firstStageClear") == 1)
-        {
-            firstStageClear = true;
-        }
-        else
-        {
-            images[1].color = Color.black;
-        }
-        
-        if (PlayerPrefs.GetInt("secondStageClear") == 1)
-        {
-            secondStageClear = true;
-        }
-        else
-        {
-            images[2].color = Color.black;
-
-        }
-        
     }
     public void Update()
     {
@@ -47,47 +27,30 @@ public class ColorChange : MonoBehaviour
         {
             if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             {
-                selected += 1;
+                selected += 1;        
+                if(selected < 1)
+                {
+                    selected = 3; 
+                }
+                if(selected > 3)
+                {
+                    selected = 1;
+                }
                 Change();
-                if(selected <= 1)
-                {
-                    selected = 1; 
-                    Change();
-                }
-
-                if (selected == 2)
-                {
-                    selected = 2; 
-                    Change();
-                }
-                if(selected <= 3)
-                {
-                    selected = 3;
-                    Change();
-                }
             
             }
             if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                selected -= 1;
-                Change();
-                if(selected >= 1)
-                {
-                    selected = 1; 
-                    Change();
-                }
-
-                if (selected == 2)
-                {
-                    selected = 2; 
-                    Change();
-                }
-                
-                if(selected <= 3)
+                selected -= 1;               
+                if(selected < 1 )
                 {
                     selected = 3;
-                    Change();
-                }                 
+                }               
+                if(selected > 3)
+                {
+                    selected = 1;
+                }
+                Change();
             }
         
             if(Input.GetKeyDown(KeyCode.Space))
@@ -99,14 +62,14 @@ public class ColorChange : MonoBehaviour
                     // GameManager.getInstance().SceneTransitionStarted();
                     // UIFader.getInstance().fadeToBlack();                
                 }
-                if(selected == 2 && firstStageClear)
+                if(selected == 2)
                 {
                     sceneTransitioning = true;
                     SceneManager.LoadScene("FireworksScene" , LoadSceneMode.Single);
                     // GameManager.getInstance().SceneTransitionStarted();
                     // UIFader.getInstance().fadeToBlack();                
                 }
-                if(selected == 3 && secondStageClear)
+                if(selected == 3)
                 {
                     sceneTransitioning = true;
                     SceneManager.LoadScene("DeathScene" , LoadSceneMode.Single);
@@ -124,8 +87,7 @@ public class ColorChange : MonoBehaviour
             images[0].color = Color.blue;
             images[1].color = Color.red;
             images[2].color = Color.red;
-            destription.text = descriptions[0];
-            // title.text = titles[0];                
+            destription.text = descriptions[0];    
         }
         if (selected == 2)
         {
@@ -133,7 +95,6 @@ public class ColorChange : MonoBehaviour
             images[0].color = Color.red;
             images[2].color = Color.red;
             destription.text = descriptions[1];
-            // title.text = titles[1];
         }
         if (selected == 3)
         {
@@ -141,8 +102,6 @@ public class ColorChange : MonoBehaviour
             images[0].color = Color.red;
             images[1].color = Color.red;
             destription.text = descriptions[2];
-            // title.text = titles[2];
-            selected = 0;
         }        
     }
 }
